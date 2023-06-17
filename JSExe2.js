bd9567783d837901f2e6ddc26f7419262695fa95
TA1028 <dina.wulan.ft21@mail.umy.ac.id>
2023-06-17 22:17:54 +0700

    my first commit

---
+++ JSExe2.js
@@ -0,0 +1,11 @@
+video.addEventListener("clik", function video(){
+    alert("Ini video Islamic");
+});
+
+audio.addEventListener("click", function audio(){
+    alert("Audio diputar");
+});
+
+gambar.addEventListener("click", function gambar(){
+    alert("ini gambar mekkah");
+});
